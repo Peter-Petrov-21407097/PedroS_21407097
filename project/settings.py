3f08a9a860f4688dfa62f2cf8440504d343074b0
Peter P. <pedro_s9@outlook.com>
2026-05-19 10:41:46 +0100

    enable whitenoise finders

--- project/settings.py
+++ project/settings.py
@@ -36,10 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     'cloudinary',
     'cloudinary_storage',
-
     'portfolio',
     'accounts',
     'artigos',
@@ -113,7 +111,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+WHITENOISE_USE_FINDERS = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
